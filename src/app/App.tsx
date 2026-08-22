9c9395e826eb381a161538508c11a38892d31aba
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-08-22 21:58:13 +0700

    feat: implement dark mode functionality with theme persistence

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,20 @@
+import DarkModeButton from '@/components/DarkModeButton';
 import MobileMenu from '../components/MobileMenu';
+import { themeStore } from '@/store/themeStore';
+import { useEffect } from 'react';
 
 function App() {
+  const isDark = themeStore((state) => state.isDark);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDark);
+  }, [isDark]);
+
   return (
-    <div className='h-1000'>
+    <>
+      <DarkModeButton />
       <MobileMenu />
-    </div>
+    </>
   );
 }
 
--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,7 +1,27 @@
+import { themeStore } from '@/store/themeStore';
 import { Button } from './ui/button';
+import { Moon, Sun } from 'lucide-react';
 
 const DarkModeButton = () => {
-  return <Button>DarkModeButton</Button>;
+  const isDark = themeStore((state) => state.isDark);
+  const toggleDark = themeStore((state) => state.toggleTheme);
+
+  const handleClickDarkMode = () => {
+    toggleDark();
+    if (!isDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  };
+  return (
+    <Button
+      onClick={handleClickDarkMode}
+      className='h-10 flex items-center justify-center'
+    >
+      {isDark ? <Sun className='size-5' /> : <Moon className='size-5' />}
+    </Button>
+  );
 };
 
 export default DarkModeButton;
